673fe9df0cc296bae7073e6ce3905d2073081b5c
Mel-Morales <83232067+Mel-Morales@users.noreply.github.com>
2023-08-26 15:55:42 +0000

    Cambié el nombre

---
+++ EjerciciosDclase/nodos.py
@@ -0,0 +1,14 @@
+class Nodo:
+    dato = None
+    apuntador = None
+    def __init__(self, dato, apuntador):
+        self.dato = dato
+        self.apuntador = apuntador
+def __str__(self):
+    return f"{self.dato}"
+
+print("="*50)
+obj1 = Nodo(5, None)
+obj2 = Nodo(5, None)
+print(obj1)
+print("otra cosa")
